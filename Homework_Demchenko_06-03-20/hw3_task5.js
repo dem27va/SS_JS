51fa27b978f6868f3c4fabf1f876c0418a5be05e
dem27va <47898484+dem27va@users.noreply.github.com>
2020-03-06 09:41:17 +0200

    Update hw3_task5.js

--- Homework_Demchenko_06-03-20/hw3_task5.js
+++ Homework_Demchenko_06-03-20/hw3_task5.js
@@ -1,18 +1,8 @@
-/*function show() {
-    round.style.background = backColor.value;
-    round.style.width = radius.value + 'em';
-    round.style.height = radius.value + 'em';
-    console.log(round.style.width);
-}*/
-
-function show() {
+function showCircle() {
     circle.style.background = backColor.value;
     circle.style.width = WiHe.value + 'em';
     circle.style.height = WiHe.value + 'em';
-    circle.style.border = 10 + 'px solid';
-    //circle.style.border-radius = WiHe.value/2 + 'em';
-    console.log(circle.style.width);
 }
 
 
-btn1.onclick = show;
+btn1.onclick = showCircle;
